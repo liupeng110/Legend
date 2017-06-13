45630f503f619817bc29f166c606531507b2401d
liupeng110 <717219917@qq.com>
2017-06-13 18:05:39 +0800

    0.0

--- sample/src/main/java/com/legend/demo/App.java
+++ sample/src/main/java/com/legend/demo/App.java
@@ -19,23 +19,21 @@ public class App extends Application {
     public static boolean ENABLE_TOAST = true;
     public static boolean ALLOW_LAUNCH_ACTIVITY = true;
 
-    @Override
-    protected void attachBaseContext(Context base) {
+    @Override protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         HookManager.getDefault().applyHooks(App.class);
-
     }
 
     @Hook("android.app.Application::onCreate")
     public static void Application_onCreate(Application app) {
-        Toast.makeText(app, "Application => onCreate()", Toast.LENGTH_SHORT).show();
+        Toast.makeText(app, "Application => onCreate()..", Toast.LENGTH_SHORT).show();
         HookManager.getDefault().callSuper(app);
     }
 
 
     @Hook("android.telephony.TelephonyManager::getSimSerialNumber")
     public static String TelephonyManager_getSimSerialNumber(TelephonyManager thiz) {
-         return "110";
+         return "SimSerialNumber--112345";
     }
 
 
@@ -43,13 +41,16 @@ public class App extends Application {
     public static void Toast_show(Toast toast) {
         if (ENABLE_TOAST) {
             HookManager.getDefault().callSuper(toast);
+        }else {
+            toast.setText("hook后的toast"); 
+            HookManager.getDefault().callSuper(toast);
         }
     }
 
     @Hook("android.app.Activity::startActivity@android.content.Intent")
     public static void Activity_startActivity(Activity activity, Intent intent) {
         if (!ALLOW_LAUNCH_ACTIVITY) {
-            Toast.makeText(activity, "I am sorry to turn your Activity down :)", Toast.LENGTH_SHORT).show();
+            Toast.makeText(activity, "I am sorry to turn your Activity down :)"+intent, Toast.LENGTH_SHORT).show();
         }else {
             HookManager.getDefault().callSuper(activity, intent);
         }
